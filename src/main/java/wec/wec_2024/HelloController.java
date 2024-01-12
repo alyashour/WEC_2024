54dc9f4aaf08f4099430045095d9b7c95837680c
Gusmahf <gusmahfoud10@gmail.com>
2024-01-12 18:37:48 -0500

    Created base GUI

--- src/main/java/wec/wec_2024/HelloController.java
+++ src/main/java/wec/wec_2024/HelloController.java
@@ -1,14 +1,60 @@
 package wec.wec_2024;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private VBox vtop;
+    @FXML
+    private VBox vbot;
+    @FXML
+    private CheckBox nameC;
+    @FXML
+    private CheckBox longC;
+    @FXML
+    private CheckBox latC;
+    @FXML
+    private CheckBox dateC;
+    @FXML
+    private CheckBox typeC;
+    @FXML
+    private CheckBox intenC;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField nameT;
+    @FXML
+    private TextField longT;
+    @FXML
+    private TextField latT;
+    @FXML
+    private TextField dateT;
+    @FXML
+    private TextField typeT;
+    @FXML
+    private TextField intenT;
+
+    @FXML
+    private void initialize() {
+        // Initially hide the text field, if the checkbox is not supposed to be checked by default.
+        nameT.managedProperty().bind(nameT.visibleProperty());
+        nameT.setVisible(nameC.isSelected());
+
+        // Add listener to the checkbox
+        nameC.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            nameT.setVisible(newValue);
+            if (newValue) {
+                // Checkbox is selected, add the text field to the container
+                if (!vbot.getChildren().contains(nameT)) {
+                    vbot.getChildren().add(nameT);
+                }
+            } else {
+                // Checkbox is deselected, remove the text field from the container
+                vbot.getChildren().remove(nameT);
+            }
+        });
     }
 }
